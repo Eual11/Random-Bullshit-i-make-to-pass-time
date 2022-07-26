768416c1c8c56acbe0c50982b12311bb94141b8d
Eual11 <uchihaeual11@gmail.com>
2022-07-27 01:06:51 +0300

    fixed the games rules and added some white outline

--- Conway's Game of Life/life.cpp
+++ Conway's Game of Life/life.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 using namespace std;
 using namespace sf;
-
+//a window with a static window size of 900x600
 RenderWindow window(VideoMode(900,600),"Life",Style::Titlebar|Style::Close);
 int main(void)
 {
@@ -11,7 +11,9 @@ int main(void)
     int x;
     int y;
     bool alive;
+    int live;
 };
+///Vertex array for somehow not visible horizontal grids lines ///
     VertexArray GridX(Lines,80);
     //settings.antialiasingLevel=10;
     for (int i=0; i<80;++i)
@@ -23,6 +25,8 @@ int main(void)
         verti.position=Vector2f(1000,(15*i));
         GridX.append(verti);
     }
+
+    ///vertex array for somehow invisible vertical grid lines
     VertexArray GridY(Lines,180);
     for (int i=0; i<180;++i)
     {
@@ -33,6 +37,7 @@ int main(void)
         verti.position=Vector2f(15*i,1000);
         GridY.append(verti);
     }
+    ///initialising a vector (dynamic array) of cells///
     vector <cell> vcell;
     window.setFramerateLimit(30);
 
@@ -40,7 +45,6 @@ int main(void)
     {
         int x=i%60;
         int y=i/60;
-        //cout <<"y: "<<y<< " i "<<i<<endl;
         cell ccell;
         ccell.x=x*15;
         ccell.y=y*15;
@@ -49,6 +53,7 @@ int main(void)
     }
     bool running =false;
     bool input=true;
+    window.setFramerateLimit(35);
     while (window.isOpen())
     {
         Event event;
@@ -69,7 +74,7 @@ int main(void)
                     int x=Mouse::getPosition(window).x/15;
                     int y=Mouse::getPosition(window).y/15;
                     //cout <<Mouse::getPosition(window).x<< " , "<<Mouse::getPosition(window).y<<endl;
-                    //cout <<(x+(y*60))<<endl;
+                    //cout <<vcell[(x+(y*60))].live<<endl;
                     vcell[(x+(y*60))].alive= !vcell[(x+(y*60))].alive;
                     break;
 
@@ -99,74 +104,74 @@ int main(void)
                 RectangleShape rcell;
                 rcell.setSize(Vector2f(15,15));
                 rcell.setFillColor(Color::Green);
+                rcell.setOutlineThickness(0.6);
+                rcell.setOutlineColor(Color::White);
                 rcell.setPosition(Vector2f(vcell[i].x,vcell[i].y));
                 window.draw(rcell);
             }
         }
-         //this_thread::sleep_for(50ms);
-
-
 
          if (running && not input)
          {
 
             for (int i=0; i<vcell.size();i++)
             {
-                int ncount=0;
+                vcell[i].live=0;
                 cell dcell=vcell[i+60];
-                
-                ncount+=dcell.alive;
+                vcell[i].live+=dcell.alive;
                 cell rcell=vcell[i+1];
-                ncount+=rcell.alive;
+                vcell[i].live+=rcell.alive;
                 cell drcell=vcell[i+61];
-                ncount+=rcell.alive;
+                vcell[i].live+=drcell.alive;
 
                 if ((i-1)>=0)
                 {
                     if (i>1) {
-                    cell lcell=vcell[i-1];
-                    ncount+=lcell.alive;
+                    cell lcell=vcell[i-1];  ///the cell to the left of the given cell vcell[i]
+                   
+                    vcell[i].live+=lcell.alive;
                     }
                     
-                    cell dlcell=vcell[i+59];
-                    ncount+=dlcell.alive;
+                    cell dlcell=vcell[i+59];  //the cell down left of the given cell
+                   
+                    vcell[i].live+=dlcell.alive;
+                    ///incrementing the number of life cells around our cell of interest by the state of the neighbor cell
 
 
                      if (i-60>=0)
                 {
                     cell ucell=vcell[i-60];
-                    ncount+=ucell.alive;
-                    //cout <<vcell[i-60].alive<<" its is i "<<i-60<<endl;;
+                    vcell[i].live+=ucell.alive;
                     cell urcell=vcell[i-59];
-                    ncount+=urcell.alive;
+                    vcell[i].live+=urcell.alive;
                     if (i>61) {
                     cell ulcell=vcell[i-61];
-                    ncount+=ulcell.alive;
-                    }
-                    
+                    vcell[i].live+=ulcell.alive;
+                    }   
                 }
-                }
-                /////the real shit beigins here/////////////
-                if (ncount<2)
-                {
-                    vcell[i].alive=false;
-                    //cout <<i<<endl;
-                }
-                if (ncount==3 && (not vcell[i].alive))
-                {
-                    vcell[i].alive=true;
-                   // cout <<i<<endl;
-                }
-                if (ncount>3)
-                {
-                    vcell[i].alive=false;
-
-                }
-               
+                }  
             }
+         for (int i=0; i<vcell.size();++i)
+         {
+            if (vcell[i].live>3 && vcell[i].alive)
+            {
+                //rule 3: any live cell with more than 3 live neighbours dies as if by overpopulation
+                vcell[i].alive=false;
+            }
+            if (vcell[i].live<2 && vcell[i].alive)
+            {
+                //rule 1: any life cell with fewer than two live cells neighbours dies as if by underpopulation
+                vcell[i].alive=false;
+            }
+            if (vcell[i].live==3 && (not vcell[i].alive)){
+
+                //rule 2: dead live cell with exactly 3 live neighbor cells becomes alive
+                vcell[i].alive=true;
+            }
+
          }
-
-
+         
+         }
         window.display();
         }
 }
